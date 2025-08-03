a54f4dc8a69e8790dc163e21410fbadfac5e12fb
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2025-08-03 17:34:03 +0900

    swagger url 테스트

--- backend/djangoApp/djangoApp/urls.py
+++ backend/djangoApp/djangoApp/urls.py
@@ -46,6 +46,10 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # API 경로의 Swagger UI
+    path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='api-schema-json'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='api-schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='api-schema-redoc'),
     # favicon 처리
     path(
         "favicon.ico",
