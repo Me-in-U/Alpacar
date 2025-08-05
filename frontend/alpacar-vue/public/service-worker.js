c131effc7e43974723a731fd43352273a1fb9ef8
김민규 <jun3021303@naver.com>
2025-08-05 12:19:04 +0900

    sevice-worker 버전 변경

--- frontend/alpacar-vue/public/service-worker.js
+++ frontend/alpacar-vue/public/service-worker.js
@@ -1,11 +1,15 @@
 // public/service-worker.js
+
+const SW_VERSION = "v2";
+
 self.addEventListener("install", (event) => {
-	console.log("Service Worker 설치 완료");
+	console.log(`Service Worker 설치 완료(${SW_VERSION})`);
 	// 예: 캐시 초기화
 });
 
 self.addEventListener("activate", (event) => {
-	console.log("Service Worker 활성화 완료");
+	event.waitUntil(self.clients.claim()); // 즉시 페이지 컨트롤
+	console.log(`Service Worker 활성화 완료(${SW_VERSION})`);
 });
 
 self.addEventListener("fetch", (event) => {
