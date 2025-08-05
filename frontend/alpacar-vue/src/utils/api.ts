f4b3b163cca8a2ff4d64c6fbd438b6f7f5926430
백종석 <baekjs0123@naver.com>
2025-08-05 14:38:07 +0900

    로그인 에러 수정

--- frontend/alpacar-vue/src/utils/api.ts
+++ frontend/alpacar-vue/src/utils/api.ts
@@ -1,3 +1,3 @@
 // src/utils/api.ts
-export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
-export const FRONTEND_BASE_URL = import.meta.env.VITE_FRONTEND_BASE_URL;
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || "https://i13e102.p.ssafy.io/api";
+export const FRONTEND_BASE_URL = import.meta.env.VITE_FRONTEND_BASE_URL || "https://i13e102.p.ssafy.io";
