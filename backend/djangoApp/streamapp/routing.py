1d252fee10f62e6d6179969472304b8f9085273f
김민규 <jun3021303@naver.com>
2025-08-06 09:32:43 +0900

    wss route pattern

--- backend/djangoApp/streamapp/routing.py
+++ backend/djangoApp/streamapp/routing.py
@@ -6,9 +6,9 @@ from streamapp.consumers import OCRTextConsumer, PiUploadConsumer, StreamConsume
 # WebSocket 엔드포인트
 websocket_urlpatterns = [
     # 라즈베리파이가 이미지/텍스트 업로드용 WebSocket 엔드포인트
-    re_path(r"ws/upload/$", PiUploadConsumer.as_asgi()),
+    re_path(r"ws/upload/?$", PiUploadConsumer.as_asgi()),
     # 관리자 대시보드용 실시간 스트림 엔드포인트
-    re_path(r"ws/stream/$", StreamConsumer.as_asgi()),
+    re_path(r"ws/stream/?$", StreamConsumer.as_asgi()),
     # OCR 번호판 텍스트 전용 WebSocket 엔드포인트
-    re_path(r"ws/text/$", OCRTextConsumer.as_asgi()),
+    re_path(r"ws/text/?$", OCRTextConsumer.as_asgi()),
 ]
